888280e76b53bec3f070abbb98adc0d1fb72b9c2
wallet616 <portfel666@gmail.com>
2015-02-28 20:26:27 +0100

    Tabelkowo

---
+++ src/jeden/tablice.java
@@ -0,0 +1,32 @@
+package jeden;
+
+public class tablice {
+	
+	static int[] tableka = new int[10];
+
+	public static void main(String[] args)
+	{
+		pie();
+		
+		tableka[0] = 5;
+		tableka[1] = 3;
+		tableka[2] = 1;
+		tableka[3] = 0;
+		tableka[4] = 45;
+		
+		System.out.println(tableka[5]);
+		
+	}
+	
+	 public static int pie()
+	 {
+		 int jeden = tableka[4] - 27;
+		 
+		 tableka[5] = jeden;
+		 
+		 System.out.println(tableka[5]);
+		 
+		 return jeden;
+	 }
+	
+}
