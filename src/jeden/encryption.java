b4ec39ccf06a6a9423077723caf978050a6da3a0
wallet616 <portfel666@gmail.com>
2015-05-11 00:34:01 +0200

    Finally.

--- src/jeden/encryption.java
+++ src/jeden/encryption.java
@@ -11,28 +11,27 @@ public class encryption {
 		String tabinput = "QTY6789!@#$%^&*UIOP5)(_+FGHŒ£¯ÑASBfg-=mw012}{ertÓê'D34qhjkló¹,zxc.vbCVn¥pR><asdZXœ?yuioJKL;NMÊ³¿Ÿñ][:WE/\\\" ";  //List of characters.
 		char[][] tab = new char[tabinput.length()][4];
 		int[] last = new int[1];
-		last[0] = 0;
 		
 		BigInteger powKey = key().pow(15);  //Key.
 		
 		for (int i = 0; i < tabinput.length(); i++)  //Assigns original character and 'code'.
 		{
 			tab[i][0] = tabinput.charAt(i);  //First column, original character.
-			tab[i][1] = powKey.toString().charAt(i);  //Second column, code assign to character.
-			tab[i][3] = "2".charAt(0);  //Second column, code assign to character.
+			tab[i][1] = "1".charAt(0);  //Second column, code assign to character.
 		}
 		for (int a = 0; a < tabinput.length(); a++)
 		{
+			tab[a][2] = powKey.toString().charAt(a);  //Second column, code assign to character.
 			last[0] = last[0] + Character.getNumericValue(tab[a][1]);  //Counts new position of character.
 			if (last[0] >= tabinput.length())  //Check if it is not more than all characters in the table.
 			{
 				last[0] = last[0] - tabinput.length();
 			}
 			
-			if (Character.getNumericValue(tab[last[0]][3]) == 1)  //Check if new position in the third column is already used. Third column is for the encrypted characters.
+			if (Character.getNumericValue(tab[last[0]][1]) == 2)  //Check if new position in the third column is already used. Third column is for the encrypted characters.
 			{
 				outerloop:
-					for (int c = last[0]; c <= tabinput.length(); c++)
+					for (int c = 0; c < tabinput.length(); c++)
 					{
 						last[0]++;  //Adds one to last[0] and checks again.
 						if (last[0] >= tabinput.length())  //Check if it is not more than characters in the table. Again.
@@ -40,18 +39,19 @@ public class encryption {
 							last[0] = last[0] - tabinput.length();
 						}
 						
-						if (Character.getNumericValue(tab[last[0]][3]) == 2)
+						if (Character.getNumericValue(tab[last[0]][1]) == 1)
 						{
-							break outerloop;  //If tab[last[0]][3] is not equal to 1, breaks the loop. Which means position is free.
+							break outerloop;  //If tab[last[0]][1] is not equal to 1, breaks the loop. Which means position is free.
 						} else {
 							continue;
 						}
 					}
 			}
-			tab[last[0]][2] = tab[a][0];  //Assigns the encrypted character in a new place in table.
-			tab[last[0]][3] = "1".charAt(0);  //Assigns 1 in a new place in table.
-		}		
+			tab[last[0]][3] = tab[a][0];  //Assigns the encrypted character in a new place in table.
+			tab[last[0]][1] = "2".charAt(0);  //Assigns 1 in third column in table.
+		}
 		
+		//Just checking.
 		System.out.println(powKey);
 		for (int x = 0; x < tabinput.length(); x++) 
 		{
